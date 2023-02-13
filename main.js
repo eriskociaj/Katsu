8941e5eb55cf903809bf4698cc62874ae1f36409
Eris Kociaj <eris.kociaj@elev.ga.ntig.se>
2023-02-14 00:59:55 +0100

    Klart kod + CSS

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /*=============== SHOW MENU ===============*/
 
-
 const navMenu = document.getElementById("nav-menu"),
       navToggle = document.getElementById("nav-toggle"),
       navClose = document.getElementById("nav-close")
